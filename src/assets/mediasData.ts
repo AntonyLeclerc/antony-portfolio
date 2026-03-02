7e130c08674dedeac3105ed476dd9d85053cbdfd
AntonyLeclerc <ptibaka@DESKTOP-3GSMA1J.localdomain>
2026-03-02 21:30:59 +0100

    trying to fix images 2

--- src/assets/mediasData.ts
+++ src/assets/mediasData.ts
@@ -11,7 +11,7 @@ export const medias = [
     description: "Parcourir mes projets sur Github",
     media: "Github",
     linkto: "https://github.com/AntonyLeclerc",
-    image: "./MediasContact/github.png",
+    image: `${import.meta.env.BASE_URL}MediasContact/github.png`,
   },
   {
     id: 3,
